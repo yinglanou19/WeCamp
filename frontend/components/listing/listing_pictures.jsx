6978819dd38c84e5de58416c6ebbfce112e21f94
Yinglan Ou <yinglanou19@gmail.com>
2019-06-05 11:44:19 -0700

    small fix

--- frontend/components/listing/listing_pictures.jsx
+++ frontend/components/listing/listing_pictures.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import "./listing_pictures.css";
 
-let slideIndex = 1;
-showDivs(slideIndex);
+// let slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-  console.log("efkeffepotkoepfkepfe");
-}
+// function plusDivs(n) {
+//   showDivs((slideIndex += n));
+//   console.log("efkeffepotkoepfkepfe");
+// }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("listing-image");
-  if (x && n > x.length) {
-    slideIndex = 1;
+// function showDivs(n) {
+//   let i;
+//   let x = document.getElementsByClassName("listing-image");
+//   if (x && n > x.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = x.length;
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   if (x && x.length > 0) {
+//     x[slideIndex - 1].style.display = "block";
+//   }
+// }
+
+function plusDivs(currIdx) {
+  let imgCollection = document.getElementsByClassName("listing-image");
+  for (let i = 0; i < imgCollection.length; i++) {
+    imgCollection[i].style.display = "none";
   }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  if (x && x.length > 0) {
-    x[slideIndex - 1].style.display = "block";
-  }
+  imgCollection[currIdx].style.display = "block";
 }
 
 export default function ListingPictures({ images }) {
   let imgCollection = [];
-
+  let currIdx = 0;
   for (let i = 0; i < images.length; i++) {
     let img = (
       <img className="listing-image" src={`${images[i]}`} key={`${i}`} />
@@ -40,14 +48,20 @@ export default function ListingPictures({ images }) {
     <div className="listing-detail-image-container">
       {imgCollection.map(img => img)}
       <button
-        className="w3-button w3-black w3-display-left"
-        onClick={() => plusDivs(-1)}
+        onClick={() => {
+          console.log("left" + currIdx);
+          currIdx = (currIdx - 1) % imgCollection.length;
+          plusDivs(currIdx);
+        }}
       >
         &#10094;
       </button>
       <button
-        className="w3-button w3-black w3-display-right"
-        onClick={() => plusDivs(1)}
+        onClick={() => {
+          console.log("right" + currIdx);
+          currIdx = (currIdx + 1) % imgCollection.length;
+          plusDivs(currIdx);
+        }}
       >
         &#10095;
       </button>
